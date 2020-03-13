f64b1a4afe1b5e7c3956ba95d8c3a456831a74c4
edervs <eedervs.97@gmail.com>
2020-03-12 18:14:58 -0600

    Is contained, is right and is left implementation

--- voronoi_diagrams/data_structures/l.py
+++ voronoi_diagrams/data_structures/l.py
@@ -2,8 +2,10 @@
 
 # Standard Library
 from typing import Any
+
 # AVL
 from . import AVLTree, AVLNode
+
 # Models
 from .models import Region, Event
 
@@ -14,27 +16,35 @@ class LNode(AVLNode):
     def __init__(self, value: Region, left=None, right=None) -> None:
         """List L AVL Node constructor."""
         super(LNode, self).__init__(value, left, right)
+        # Just renaming for clarity.
+        self.region: Region = self.value
 
     def is_contained(self, value: Event, *args: Any, **kwargs: Any) -> bool:
         """Value is contained in the Node."""
-        # TODO: Depending on a y value
-        # TODO: Check that the left boundary is to the left of the self.value and the
-        # right boundary is to the right of the self.value.
-        return value == self.value
+        point = value.point
+        is_left_contained = True
+        is_right_contained = True
+        if self.region.left is None and self.region.right is None:
+            return True
+        if self.region.right is not None:
+            is_right_contained = self.region.right.get_point_comparison(point) <= 0
+        if self.region.left is not None:
+            is_left_contained = self.region.left.get_point_comparison(point) >= 0
+        return is_left_contained and is_right_contained
 
     def is_left(self, value: Event, *args: Any, **kwargs: Any) -> bool:
         """Value is to the left of Node."""
-        # TODO: Depending on a y value
-        # TODO: Check that the left boundary is to the right of the self.value and the
-        # right boundary is to the right of the self.value.
-        return value < self.value
+        if self.region.left is None:
+            return False
+        point = value.point
+        return self.region.left.get_point_comparison(point) < 0
 
     def is_right(self, value: Event, *args: Any, **kwargs: Any) -> bool:
         """Value is to the right of Node."""
-        # TODO: Depending on a y value
-        # TODO: Check that the left boundary is to the left of the self.value and the
-        # right boundary is to the left of the self.value.
-        return value > self.value
+        if self.region.right is None:
+            return False
+        point = value.point
+        return self.region.right.get_point_comparison(point) > 0
 
 
 class ListL:
