0d83492aff37286813305fc9a205b31d6b26a183
edervs <eedervs.97@gmail.com>
2020-09-02 17:23:58 -0500

    Changing step in boundaries

--- plots/plot_utils/models/boundaries.py
+++ plots/plot_utils/models/boundaries.py
@@ -28,15 +28,12 @@ def get_plot_scatter_boundary(
 ) -> go.Scatter:
     """Plot boundary."""
     y_list = []
+    step = abs(xlim[1] - xlim[0]) / Decimal("1000")
     if bisector_class == PointBisector:
         if boundary.sign:
-            x_list = np.arange(
-                boundary.get_site().point.x, Decimal(xlim[1]), Decimal("0.01")
-            )
+            x_list = np.arange(boundary.get_site().point.x, Decimal(xlim[1]), step)
         else:
-            x_list = np.arange(
-                Decimal(xlim[0]), boundary.get_site().point.x, Decimal("0.01")
-            )
+            x_list = np.arange(Decimal(xlim[0]), boundary.get_site().point.x, step)
         for x in x_list:
             if x < xlim[0] or x > xlim[1]:
                 y_list.append(None)
@@ -54,15 +51,11 @@ def get_plot_scatter_boundary(
             change_of_x = boundary.bisector.conic_section.get_changes_of_sign_in_x()[0]
             x_lists = [[], []]
             if boundary.sign:
-                x_lists[0] = np.arange(
-                    boundary.get_site().point.x, change_of_x, Decimal("0.01")
-                )
-                x_lists[1] = np.arange(change_of_x, Decimal(xlim[0]), Decimal("-0.01"))
+                x_lists[0] = np.arange(boundary.get_site().point.x, change_of_x, step)
+                x_lists[1] = np.arange(change_of_x, Decimal(xlim[0]), -step)
             else:
-                x_lists[0] = np.arange(
-                    boundary.get_site().point.x, change_of_x, Decimal("-0.01")
-                )
-                x_lists[1] = np.arange(change_of_x, Decimal(xlim[1]), Decimal("0.01"))
+                x_lists[0] = np.arange(boundary.get_site().point.x, change_of_x, -step)
+                x_lists[1] = np.arange(change_of_x, Decimal(xlim[1]), step)
 
             func_map = {0: min, 1: max}
             for i, _ in enumerate(x_lists):
@@ -82,13 +75,9 @@ def get_plot_scatter_boundary(
             x_list = np.concatenate(x_lists)
 
         elif boundary.sign:
-            x_list = np.arange(
-                boundary.get_site().point.x, Decimal(xlim[1]), Decimal("0.01")
-            )
+            x_list = np.arange(boundary.get_site().point.x, Decimal(xlim[1]), step)
         else:
-            x_list = np.arange(
-                Decimal(xlim[0]), boundary.get_site().point.x, Decimal("0.01")
-            )
+            x_list = np.arange(Decimal(xlim[0]), boundary.get_site().point.x, step)
 
         if y_list == []:
             for x in x_list:
