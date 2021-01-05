4e8c50dde94d4b86b67304597cbc8cc5a505b124
edervs <eedervs.97@gmail.com>
2021-01-05 08:23:49 -0600

    refactoring types in point.py

--- voronoi_diagrams/models/points.py
+++ voronoi_diagrams/models/points.py
@@ -1,34 +1,36 @@
 """Site Representation."""
 # Standard Library
-from typing import Any
+from typing import Tuple
 
 # Math
 from decimal import Decimal
 
+Coordinates = Tuple[Decimal, Decimal]
+
 
 class Point:
-    """Point to handle in Fortune's Algorithm."""
+    """Point representation."""
 
     x: Decimal
     y: Decimal
 
-    def __init__(self, x, y):
+    def __init__(self, x: Decimal, y: Decimal) -> None:
         """Construct point."""
         self.x = x
         self.y = y
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Get String representation."""
         return f"{'{0:.4f}'.format(self.x)}, {'{0:.4f}'.format(self.y)}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Get object representation."""
         return self.__str__()
 
-    def __eq__(self, point: Any):
+    def __eq__(self, point: "Point") -> str:
         """Get equality between points."""
         return self.x == point.x and self.y == point.y
 
-    def get_tuple(self):
+    def get_tuple(self) -> Coordinates:
         """Get tuple of coordinates (x, y)."""
         return (self.x, self.y)
